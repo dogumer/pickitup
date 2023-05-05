dd7a936951859fc001def44d630ccb38fc6c46a0
dogumer <dogumer2@gmail.com>
2023-05-05 16:35:50 +0300

    Move extension data to extensions.json

--- pickitup/picker.py
+++ pickitup/picker.py
@@ -2,22 +2,11 @@ import os
 import shutil
 import time
 import calendar
+import json
 
 def program():
-    types = {
-        "Archive" : {".7z",".a",".apk",".ar",".bz2",".cab",".cpio",".deb",".dmg",".egg",".gz",".iso",".jar",".lha",".mar",".pea",".rar",".rpm",".s7z",".shar",".tar",".tbz2",".tgz",".tlz",".war",".whl",".xpi",".zip",".zipx",".xz",".pak"},
-        "Audio" : {".aac",".aiff",".ape",".au",".flac",".gsm",".it",".m3u",".m4a",".mid",".mod",".mp3",".mpa",".pls",".ra",".s3m",".sid",".wav",".wma",".xm"},
-        "Book" : {".mobi",".epub",".azw1",".azw3",".azw4",".azw6",".azw",".cbr",".cbz"},
-        "Code" : {".ada",".adb",".ads",".asm",".bas",".bash",".bat",".c",".c++",".cbl",".cc",".class",".clj",".cob",".cpp",".cs",".csh",".cxx",".d",".diff",".e",".el",".f",".f77",".f90",".fish",".for",".fth",".ftn",".go",".groovy",".h",".hh",".hpp",".hs",".htm",".hxx",".java",".js",".jsx",".jsp",".ksh",".kt",".lhs",".lisp",".lua",".m",".m4",".nim",".patch",".php",".pl",".po",".pp",".py",".r",".rb",".rs",".s",".scala",".sh",".swg",".swift",".v",".vb",".vcxproj",".xcodeproj",".xml",".zsh"},
-        "Executable" : {".exe",".msi",".bin",".command",".sh",".bat",".crx",".bash",".csh",".fish",".ksh",".zsh"},
-        "Font" : {".eot",".otf",".ttf",".woff",".woff2"},
-        "Image" : {".3dm",".3ds",".max",".bmp",".dds",".gif",".jpg",".jpeg",".png",".psd",".xcf",".tga",".thm",".tif",".tiff",".yuv",".ai",".eps",".ps",".svg",".dwg",".dxf",".gpx,",".kml",".kmz",".webp"},
-        "Sheet" : {".ods",".xls",".xlsx",".csv",".ics",".vcf"},
-        "Slide" : {".ppt",".odp"},
-        "Text" : {".doc",".docx",".ebook",".log",".md",".msg",".odt",".org",".pages",".pdf",".rtf",".rst",".tex",".txt",".wpd",".wps"},
-        "Video" : {".3g2",",.3gp",".aaf",".asf",".avchd",".avi",".drc",".flv",".m2v",".m4p",".m4v",".mkv",".mng",".mov",".mp2",".mp4",".mpe",".mpeg",".mpg",".mpv",".mxf",".nsv",".ogg",".ogv",".ogm",".qt",".rm",".rmvb",".roq",".srt",".svi",".vob",".webm",".wmv",".yuv"},
-    "Web" : {".html",".htm",".css",".js",".jsx",".less",".scss",".wasm",".php"}
-    }
+    with open('extensions.json') as file:
+        extensions = json.load(file) 
 
     files = os.listdir(os.curdir)
     filelist = []
